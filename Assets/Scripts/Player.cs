ad2f857791022e1134f5f00eca222bac2c03710f
Moritz <22204103+momower1@users.noreply.github.com>
2019-02-13 18:44:13 +0100

    Added new scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity;
+
+    protected void Start()
+    {
+        
+    }
+
+    protected void Update()
+    {
+        
+    }
+}
